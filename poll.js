786a3b665750c6b7999b54f418913eb4275df11b
Will <william.j.villanueva@gmail.com>
2014-12-19 18:47:02 -0800

    Updated poll.js to exect scp

--- poll.js
+++ poll.js
@@ -1,6 +1,9 @@
 // Gets the cumulative number of total requests from the site-pronto staging server
 
+var fs = require('fs');
+var sys = require('sys');
 var http = require('http');
+var exec = require('child_process').exec;
 
 setInterval(function() {
   http.get("http://prontostg001iad.io.askjeeves.info:8001/metrics",
@@ -13,7 +16,16 @@ setInterval(function() {
       res.on('end', function() {
         var data = JSON.parse(body);
         var count = (data['site-pronto'].request.rate.count);
-        console.log(count);
+        fs.writeFile("data", JSON.stringify(count), function(err) {
+          if(err) {
+            console.log(err);
+          } else {
+            exec('sshpass -p timemachine scp data root@104.236.148.218:/tmp/',
+              function (error, stdout, stderr) {
+                console.log(error);
+            });
+          }
+        }); 
       });
     }).on('error', function(e) {
     console.log("Got error: " + e.message);
